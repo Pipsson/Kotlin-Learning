85270198e3cd09beaa1c72426b545749a281bb60
Pipsson <juliusmallya8420@gmail.com>
2026-05-03 10:30:24 +0300

    If statement branch

---
+++ if-statements.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val trafficLightColor = "Red"
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Please slow down")
+    } else if (trafficLightColor == "Green" ){
+        println("Go")   
+    }
+    else{
+        println("Invalid traffic light color")
+    }
+}
